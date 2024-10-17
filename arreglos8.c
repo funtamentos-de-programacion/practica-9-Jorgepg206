715a9b7d1e90719977219a6377814bd2b57b8c2a
Pacheco Garcia Jorge <jorgepg206@gmail.com>
2024-10-17 10:45:32 -0600

    Solucion practica 10

--- arreglos8.c
+++ arreglos8.c
@@ -6,10 +6,20 @@
 //  @return varible de tipo int
 //  @param una matriz
 //  @param el número de filas
+int suma (int a[][3], int f);
+int suma2(int **a, int f, int c);
 
 
 int main() {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    int res=suma(arr, 3); //se puede dejar SOLO el primer termina vacio
+    int *ptr = &res;    //(23)
+
+    printf("%d\t", *(ptr));  //(24)
+
+    // No funciona la linea porque se requiere que arr se escriba como segundo apuntador pero no se puede res=suma2(arr, 3, 3);
+    printf("%d\t", *(ptr));
 
     // TODO: (23) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
     // pasa a la función el arreglo y su tamaño
@@ -19,6 +29,23 @@ int main() {
     
     return 0;
 }
-
-
 //TODO: (25) Crea la función suma que realiza la suma de todos los elementos de un arreglo
+int suma(int a[][3], int f){
+    int r=0;
+    for (int i = 0; i < f; i++) {
+        for (int j = 0; j < 3; j++) {
+            r += a[i][j];
+        }
+    }
+    return r;
+}
+
+int suma2(int **a, int f, int c){
+    int r=0;
+    for (int i = 0; i < f; i++) {
+        for (int j = 0; j < c; j++) {
+            r += a[i][j];
+        }
+    }
+    return r;
+}
